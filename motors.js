3ed170d47e54b5a0b952eaacd11d4a7712af3829
Sam Meech-Ward <sam@meech-ward.com>
2018-06-03 18:16:57 -0700

    Export the turn on and off functions

--- motors.js
+++ motors.js
@@ -6,8 +6,10 @@ function turnOn() {
   motor1.write(1);
   motor2.write(1);
 }
+exports.turnOn = turnOn;
 
 function turnOff() {
   motor1.write(0);
   motor2.write(0);
 }
+exports.turnOff = turnOff;
